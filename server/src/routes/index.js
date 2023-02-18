8a4077e86059bdcdbd6556c06bee77b96041be6b
Guilherme Inoe <guilhermeinoe82@gmail.com>
2023-02-18 14:21:04 -0300

    reorganizando os arquivos

--- server/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import carRoutes from "./car.routes.js";
-
-const routes = app => {
-    carRoutes(app);
-};
-
-export default routes;
--- server/src/validations/car.validation.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const carValidation = yup.object({
-    veiculo: yup.string().required(),
-    marca: yup.string().required(),
-    ano: yup.number().required(),
-    desc: yup.string().required(),
-    vendido: yup.boolean().required(),
-});
